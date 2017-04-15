c0c692ed4eb9ca68e800a9bc2343708fabb0ad60
stratigos <stratigos@users.noreply.github.com>
2017-04-15 16:53:26 -0400

    FIRST

---
+++ lib/bank_statement.ex
@@ -0,0 +1,18 @@
+defmodule BankStatement do
+  @moduledoc """
+  Documentation for BankStatement.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BankStatement.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bank_statement_test.exs
@@ -0,0 +1,8 @@
+defmodule BankStatementTest do
+  use ExUnit.Case
+  doctest BankStatement
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
